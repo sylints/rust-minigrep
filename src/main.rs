97603b6427c76c8e2575d58f047ee40f14906181
Herby <sylints@gmail.com>
2018-09-24 22:13:00 -0600

    from pc

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::fs;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
-    println!("{} {}", config.query, config.filename);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguements: {}", err);
+        process::exit(1);
+    });
 }
 
 struct Config {
@@ -13,9 +16,15 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(x: &[String]) -> Config {
-    let query = x[1].clone();
-    let filename = x[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguements");
+        }
 
-    Config { query, filename }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
